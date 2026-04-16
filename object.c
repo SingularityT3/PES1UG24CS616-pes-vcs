96fb946b4b67505fa2754f9ba3245570ec57dc16
SingularityT3 <44658109+SingularityT3@users.noreply.github.com>
2026-04-16 10:38:18 +0530

    Create header

--- object.c
+++ object.c
@@ -94,8 +94,27 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
+    char header[64];
+    const char *type_str;
+
+    switch (type) {
+        case OBJ_BLOB:
+            type_str = "blob";
+            break;
+        case OBJ_TREE:
+            type_str = "tree";
+            break;
+        case OBJ_COMMIT:
+            type_str = "commit";
+            break;
+        default:
+            return -1;
+    }
+
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len) + 1;
+
+    printf("HEADER:\n%s\nLen: %d\n", header, header_len);
+
     return -1;
 }
 
